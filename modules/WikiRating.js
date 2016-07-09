592a87892f16e64aa7574ce520c8a65e8e1020d6
Abhimanyu Shekhawat <abhimanyu.bitsgoa@gmail.com>
2016-07-10 02:43:06 +0530

    Incorporated 'mw' wrapper to get the current Page and User information

--- modules/WikiRating.js
+++ modules/WikiRating.js
@@ -1,10 +1,27 @@
 $( document ).ready(function () {
-    
 
-    var $rating = $('<progress value="50" max="100" style="float: left ">33%</progress>');
-    var $ratingText=$('<p align=centre>Page Rating</p>');
-    $ratingText.appendTo(document.getElementById("siteSub"));
+
+    var $rating = $('<progress value="66" max="100" >33%</progress><br>');
+    var $ratingText="Page Rating: ";
+    $( "#siteSub" ).append($ratingText);
     $rating.appendTo(document.getElementById("siteSub"));
+    $( "#siteSub" ).append("Title: "+mw.config.get("wgPageName")+"<br>"+" User: "+mw.config.get("wgUserName")+"<br>");
+
+    var badgeImage = $('<img />', {src : 'extensions/WikiRating/modules/im.png'});
+    badgeImage.appendTo('#siteSub');
+
+    //var $ratingText=$('<p align=centre>Page Rating</p>');
+    //var $ratingText="hello I am good";
+    /*$ratingText.appendTo(document.getElementById("siteSub"));
+    $rating.appendTo(document.getElementById("siteSub"));*/
+    //$testContent=load("www.google.com");
+    //$testContent.appendTo(document.getElementById("contentSub"));
+    //http://www.json-generator.com/api/json/get/crcEHvmyZK?indent=2
+    //$( "#contentSub" ).load( "extensions/WikiRating/modules/sc.php" );
+
+    //$( "#contentSub" ).text(mw.config.get("wgArticleId"));
+    //$( "#contentSub" ).text(mw.config.get("wgUserId"));
+
+
 
     });
-
